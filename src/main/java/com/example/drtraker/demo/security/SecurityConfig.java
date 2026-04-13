94b39decb8a5f1216d7e6b2010d16497df5c1ed5
sujithsgit <sujith2819sr@gmail.com>
2026-04-13 06:45:07 +0530

    fix cors

--- src/main/java/com/example/drtraker/demo/security/SecurityConfig.java
+++ src/main/java/com/example/drtraker/demo/security/SecurityConfig.java
@@ -37,7 +37,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://ds-tracker-frontend.vercel.app" 
+            ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
